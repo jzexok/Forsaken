0cc5292bdb5392855583e523fbf03deb95d7c39d
jzexok <smoothxeliturhan@gmail.com>
2025-06-01 10:56:35 +0300

    Update Tracers.lua

--- ESP/Player/Tracers.lua
+++ ESP/Player/Tracers.lua
@@ -1,7 +1,7 @@
 local players = game:GetService("Players")
 local run_service = game:GetService("RunService")
-local local_player = players.LocalPlayer
 local camera = workspace.CurrentCamera
+local local_player = players.LocalPlayer
 
 if getgenv().cyberline_tracers then
 	for _, line in pairs(getgenv().cyberline_tracers) do
@@ -11,9 +11,9 @@ end
 
 getgenv().cyberline_tracers = {}
 
-local function create_tracer()
+local function create_tracer(color)
 	local line = Drawing.new("Line")
-	line.Color = Color3.fromRGB(255, 255, 255)
+	line.Color = color
 	line.Thickness = 1
 	line.Transparency = 1
 	line.Visible = false
@@ -21,26 +21,32 @@ local function create_tracer()
 end
 
 run_service.RenderStepped:Connect(function()
-	for _, player in ipairs(players:GetPlayers()) do
-		if player ~= local_player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-			local root_part = player.Character.HumanoidRootPart
-			local screen_pos, on_screen = camera:WorldToViewportPoint(root_part.Position)
+	for _, group in pairs({
+		{ folder = workspace.Players.Killers, color = Color3.fromRGB(255, 0, 0) },
+		{ folder = workspace.Players.Survivors, color = Color3.fromRGB(255, 255, 255) },
+	}) do
+		for _, char in ipairs(group.folder:GetChildren()) do
+			local player = players:GetPlayerFromCharacter(char)
+			if player and player ~= local_player and char:FindFirstChild("HumanoidRootPart") then
+				local root = char.HumanoidRootPart
+				local screen_pos, on_screen = camera:WorldToViewportPoint(root.Position)
 
-			if not getgenv().cyberline_tracers[player] then
-				getgenv().cyberline_tracers[player] = create_tracer()
+				if not getgenv().cyberline_tracers[player] then
+					getgenv().cyberline_tracers[player] = create_tracer(group.color)
+				end
+
+				local tracer = getgenv().cyberline_tracers[player]
+
+				if on_screen then
+					tracer.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y)
+					tracer.To = Vector2.new(screen_pos.X, screen_pos.Y)
+					tracer.Visible = true
+				else
+					tracer.Visible = false
+				end
+			elseif player and getgenv().cyberline_tracers[player] then
+				getgenv().cyberline_tracers[player].Visible = false
 			end
-
-			local tracer = getgenv().cyberline_tracers[player]
-
-			if on_screen then
-				tracer.From = Vector2.new(camera.ViewportSize.X / 2, camera.ViewportSize.Y)
-				tracer.To = Vector2.new(screen_pos.X, screen_pos.Y)
-				tracer.Visible = true
-			else
-				tracer.Visible = false
-			end
-		elseif getgenv().cyberline_tracers[player] then
-			getgenv().cyberline_tracers[player].Visible = false
 		end
 	end
 end)
